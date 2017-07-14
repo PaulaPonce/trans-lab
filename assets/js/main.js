6ecfb339f23e0d2644815027c67e961bbf8cdea4
Paula Ponce <paula.poncez@usach.cl>
2017-07-14 01:59:51 -0400

    save and show email using local storage

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,28 +1,48 @@
 $(document).ready(function(){
+	//localStorage
+	loadSettings();
+
 	//Initialize SideNav
 	$("#activate-sideNav").sideNav();
 
+	
+
 	//Index validation
 	$("#sign-in").on("click", validateForm);
+	var email;
+	var pass;
 
 	function validateForm(event){
 		event.preventDefault();
-		var email = $("#email").val();
-		var pass = $("#pass").val();
-
+		email = $("#email").val();
+		pass = $("#pass").val();
 		if(!/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email)){
 			$(".email-container").append("<label class='alert-label'>Verifique su email</label>");
 		}else if(!/[0-9]/.test(pass) || pass.length >= 8){
 			$(".password-container").append("<label class='alert-label'>Máximo 8 caracteres. Solo se permiten números</label>");
 		}else{
 			window.location.href="home.html";
+			saveSettings();
 		}
-		clear();
+		//clear();
 	}
 	//limpiar campos
+	/*
 	function clear(){
 		$(":input")
 		.not(":button, :submit, :reset, :hidden")
 		.val("")
-	}
+	}*/
+
+	
 })
+
+//localStorage
+function loadSettings() {
+	$("#index-email").append("<span class='email white'>" + localStorage.email + "</span>");
+	//$("#email").val(localStorage.correo);
+}
+
+function saveSettings() {
+	localStorage.email = $("#email").val();
+}
